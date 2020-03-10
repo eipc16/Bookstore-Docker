d578588de8e92f27b22db54cbcb5c7ad2eab99db
pietrzakp <epicki16@gmail.com>
2020-03-10 20:55:11 +0100

    Completed Exercise 2

---
+++ zadanie_2/zad2.js
@@ -0,0 +1,15 @@
+function sum(numbers) {
+    let result = 0
+    let currentValue;
+    for (let i = 0; i < numbers.length; i++) {
+        currentValue = parseInt(numbers[i])
+        if (currentValue % 2 == 0) {
+            result += currentValue - 1
+        } else {
+            result += currentValue + 1
+        }
+    }
+    return result
+}
+
+print(sum(arguments))
